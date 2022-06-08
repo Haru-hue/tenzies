bf8719b488d92edeb6cfdfb83060c0b250fc5a67
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-06-08 11:40:05 +0100

    Added the leaderboard

--- frontend/src/components/Scores.jsx
+++ frontend/src/components/Scores.jsx
@@ -1,25 +1,55 @@
 import { useEffect, useState } from "react"
+import pin from "../pin.png"
 
-const Scores = (props) => {
-    const [data, setData] = useState([])    
+const Scores = () => {
+    const [results, setResults] = useState({
+        username: '',
+        time: '',
+        rolls: 0
+    })
 
     useEffect(() => {
         async function callAPI () {
-            await fetch("http://localhost:5000/")
-            .then(res => res.json())
-            .then(res => setData(res))
+            const res = await fetch("http://localhost:5000/")
+            const data = await res.json()
+            console.log(data)
+            data.map(item => {
+                   setResults({
+                     username: item.username,
+                     time: item.totalTime,
+                     rolls: item.totalRounds
+                 })
+             })
         }  
 
         callAPI()
     }, [])
 
-    console.log(data)
 
     return (
         <div className="leaderboard">
-            hello
+            <figure>
+                <img src={pin} alt="Hello pin"/>
+            </figure>
+            <h2>Leaderboard</h2>
+            <table>
+                <thead>
+                    <th>Username</th>
+                    <th>Total Time</th>
+                    <th>Total Rounds</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{results.username}</td>
+                        <td>{results.time}</td>
+                        <td>{results.rolls}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
 
 export default Scores
+
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import Tenzie from "./Tenzie"
+import App from "./App"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
-root.render(<Tenzie/>)
+root.render(<App/>)
