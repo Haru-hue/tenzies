6b183f8dffc42a45d832c06114fc913c476d64a3
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-06-09 11:51:54 +0100

    Frontend

---
+++ src/components/Scores.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+import pin from "../pin.png"
+
+const Scores = () => {
+    const [results, setResults] = useState([])
+
+    useEffect(() => {
+        async function callAPI () {
+            const res = await fetch("http://localhost:5000/")
+            const data = await res.json()
+            console.log(data)
+            setResults(data)
+        }  
+
+        callAPI()
+    }, [])
+
+    const resultArray = results.map(item => {
+        return (
+            <tr>
+                <td>{item.username}</td>
+                <td>{item.totalTime}</td>
+                <td>{item.totalRounds}</td>
+            </tr>
+        )
+    })   
+
+    return (
+        <div className="leaderboard">
+            <figure>
+                <img src={pin} alt="Hello pin"/>
+            </figure>
+            <h2>Leaderboard</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Total Time</th>
+                        <th>Total Rounds</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   {resultArray}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Scores
